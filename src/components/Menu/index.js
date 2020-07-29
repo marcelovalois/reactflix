730b0b6714288fe3d1e0fba73f7c2b3e9d50f802
Marcelo Valois <mmv4@cin.ufpe.br>
2020-07-28 21:16:15 -0300

    Segundo dia

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+
 import Logo from '../../assets/img/Logo.png';
 import './Menu.css';
 import Button from '../Button'
@@ -7,10 +9,10 @@ import Button from '../Button'
 function Menu() {
     return (
       <nav className="Menu">
-        <a href="/">
+        <Link to="/">
           <img className="Logo" src={Logo} alt="AluraFlix logo"></img>
-        </a>
-        <Button as="a" className="ButtonLink" href="/">
+        </Link>
+        <Button as={Link} className="ButtonLink" to="/cadastro/video">
           Novo vídeo
         </Button>
       </nav>
